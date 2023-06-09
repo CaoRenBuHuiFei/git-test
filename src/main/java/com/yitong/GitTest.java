d53357ca1f271b94c91d00735d0ea94c37e9f129
wdf <2119227017@qq.com>
2023-06-09 21:03:17 +0800

    push

--- src/main/java/com/yitong/GitTest.java
+++ src/main/java/com/yitong/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hot");
         System.out.println("master");
+        System.out.println("push");
     }
 }
